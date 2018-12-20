760f9f936441b66fb3c10438c6bbe4229f9041c1
Spencer Riley <spencerr@slalom.com>
2018-12-20 14:23:37 -0700

    component complete

---
+++ force-app/main/default/aura/ContactAlert/ContactAlertController.js
@@ -0,0 +1,22 @@
+({
+    fireToastEvent : function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        var contact = component.get("v.record");
+        toastEvent.setParams({
+            "title": "Alert",
+            "message": contact.Alert_Text__c,
+            "type": "success"
+        });
+        toastEvent.fire();
+    },
+    recordLoaded : function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        var contact = component.get("v.record");
+        toastEvent.setParams({
+            "title": "Alert",
+            "message": contact.Alert_Text__c,
+            "type": "success"
+        });
+        toastEvent.fire();
+    }
+})
